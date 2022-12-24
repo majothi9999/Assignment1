e2df7c03f4f5a7bcfd71ec0409a8b9a8a72b20d1
majothi9999 <110264390+majothi9999@users.noreply.github.com>
2022-12-24 01:37:31 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,111 @@
+var a = {
+  samsungmobiles: {
+    samsung: {
+      a30: {
+        name: "a30",
+        ram: "4gb",
+        rom: "64gb",
+        camera: "50px",
+        price: 2000,
+      },
+      note10: {
+        name: "note10",
+        ram: "6gb",
+        rom: "128gb",
+        camera: "50px",
+        price: 4000,
+      },
+      s10: {
+        name: "s10",
+        ram: "3gb",
+        rom: "128gb",
+        camera: "10px",
+        price: 5000,
+      },
+    },
+  },
+};
+
+
+function searchVal() {
+  console.log(a['samsungmobiles'] );
+}
+
+var b ={
+    iphonemobiles: {
+        iphone: {
+                    iphone4: {
+                      name: "iphone4",
+                      ram: "4gb",
+                      rom: "64gb",
+                      camera: "50px",
+                      price: 10000,
+                    },
+                    iphone4s: {
+                      name: "iphone4s",
+                      ram: "4gb",
+                      rom: "64gb",
+                      camera: "50px",
+                      price: 20000,
+                    },
+                    iphone5: {
+                      name: "iphone5",
+                      ram: "4gb",
+                      rom: "64gb",
+                      camera: "50px",
+                      price: 30000,
+                    },
+                    iphone6: {
+                      name: "iphone6",
+                      ram: "4gb",
+                      rom: "64gb",
+                      camera: "50px",
+                      price: 30000,
+                    },
+                  },
+    }
+}
+
+function searchNub() {
+    console.log(b['iphonemobiles'] );
+  }
+
+
+  var c ={
+    redmimobiles:{
+        Redmi: {
+                    redminote10: {
+                      name: "redminote10",
+                      ram: "4gab",
+                      rom: "64gb",
+                      camera: "50px",
+                      price: 10000,
+                    },
+                    redminote11: {
+                      name: "redminote11",
+                      ram: "4gb",
+                      rom: "64gb",
+                      camera: "50px",
+                      price: 10000,
+                    },
+                    redmi10: {
+                      name: "redmi10",
+                      ram: "4gb",
+                      rom: "64gb",
+                      camera: "50px",
+                      price: 10000,
+                    },
+                    redmi10pro: {
+                      name: "redmi10pro",
+                      ram: "4gb",
+                      rom: "64gb",
+                      camera: "50px",
+                      price: 10000,
+                    },
+                  },
+                },
+    }
+  
+    function searchVals() {
+        console.log(c['redmimobiles'] );
+      }
